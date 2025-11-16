51fb0e14423e876787df89973300fc2fad4c0f33
OguraBike <yoshidago@oburabike.com>
2025-11-16 22:01:37 +0900

    no message

---
+++ src/main/java/com/example/springbootprojectanalyser/SpringbootprojectanalyserApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootprojectanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootprojectanalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootprojectanalyserApplication.class, args);
+	}
+
+}
